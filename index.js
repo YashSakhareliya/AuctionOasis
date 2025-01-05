029862a9f8ec806b1868a5900cfc86f796ee1bee
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-05 13:07:42 +0530

    EJS was run successfully

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('test')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening localhost:${port}`)
 })
