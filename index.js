7f94d3aa718567ada0bf3739063c25c613c24e78
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-05 13:47:50 +0530

    File structure was update based on ejs format

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
-
 const express = require('express')
+const path = require('path')
+
+
 const app = express()
 const port = process.env.PORT
 
-app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));   // for all css and javascript files
+app.set('view engine', 'ejs')   // told to express my frontend is in EJS file
+
+
 app.get('/', (req, res) => {
-    res.render('test')
+    res.render('index')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening localhost:${port}`)
