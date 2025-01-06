7b7b338aed9cbe3cf34f70d6c07c372bd9aab4bc
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-06 23:14:02 +0530

    Data base connection code added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
+const { render } = require('ejs')
 const express = require('express')
+const bodyParser = require("body-parser");
 const path = require('path')
 
 
@@ -8,16 +10,30 @@ const port = process.env.PORT
 
 
 app.use(express.static(path.join(__dirname, 'public')));   // for all css and javascript files
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')   // told to express my frontend is in EJS file
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 app.get('/login', (req, res) => {
     res.render('auth/login')
 })
 
-app.get('/', (req, res) => {
+app.post('/login', (req, res) => {
+  const {username , password} = req.body
+  console.log(username, password)
   res.render('index')
 })
+
+
+app.get('/signout', (req, res) => {
+  res.render('auth/login')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening localhost:${port}`)
