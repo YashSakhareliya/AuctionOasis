1dfd9e46ce1bbac6a459673901674c8a0332d91c
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-12 13:14:12 +0530

    Register user logic done

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { readFile, writeFile } = require('../utils/fileHandler');
 
 const loginFilePath = path.join(__dirname, '../files/login.json');
+const registeruserFilePath = path.join(__dirname, '../files/registeruser.json');
 
 const renderLogin = (req, res) => {
   res.render('auth/login', { title: 'Login' });
@@ -29,9 +30,22 @@ const renderRegister = (req, res) => {
 
 const registerUser = (req, res) => {
   const { username, email, password, conformpassword } = req.body;
+  const newUser = { username, email, password}
 
   if (password !== conformpassword) {
     return res.send('Passwords do not match');
+  }
+
+  const registerUser  = readFile(registeruserFilePath)
+
+  const existUser = registerUser.some(user=>user.username === newUser.username)
+
+  if(existUser){
+    return res.send('Username already exists');
+  }else{
+    registerUser.push(newUser)
+    writeFile(registeruserFilePath, registerUser)
+    res.render('auth/login', { title: 'Login' });
   }
 
   console.log({ username, email, password });
