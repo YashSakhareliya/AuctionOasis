f505e4d3f170a66a734eac33532333971a3ad8db
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-12 21:46:11 +0530

    verify tocken logindone

---
+++ Middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next)=>{
+    const token = req.cookies.auth_token
+
+    if(!token){
+        res.locals.isAuthenticated = false
+        return next()
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        res.locals.isAuthenticated = true
+        res.locals.username = decoded.username
+        next()
+    }catch(err){
+        res.locals.isAuthenticated = false
+        return next()
+    }
+}
+
+
+module.exports = {verifyToken};
