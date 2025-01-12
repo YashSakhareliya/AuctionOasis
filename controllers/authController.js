919002470bd378376ba0f382092683dd697f961d
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-12 21:48:47 +0530

    verify user Logic implemeted

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,12 @@
+const jwt = require('jsonwebtoken');
 const path = require('path');
 const { readFile, writeFile } = require('../utils/fileHandler');
 
 const loginFilePath = path.join(__dirname, '../files/login.json');
 const registeruserFilePath = path.join(__dirname, '../files/registeruser.json');
+
+// JWT Secret Key
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const renderLogin = (req, res) => {
   res.render('auth/login', { title: 'Login' });
@@ -23,6 +27,12 @@ const loginUser = (req, res) => {
   }else{
 
     if(loginUserExist.password === loginUser.password){
+
+      // Generate JWT
+      const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' });
+
+      // Set token in a secure cookie
+      res.cookie('auth_token', token, { httpOnly: true, secure: true });
 
       let data = readFile(loginFilePath);
 
@@ -65,11 +75,12 @@ const registerUser = (req, res) => {
   }
 
   console.log({ username, email, password });
-  res.render('auth/login', { title: 'Login' });
+  res.redirect('auth/login');
 };
 
 const signOut = (req, res) => {
-  res.render('auth/login', { title: 'Login' });
+  res.clearCookie('auth_token');
+  res.redirect('/auth/login');
 };
 
 module.exports = { renderLogin, loginUser, renderRegister, registerUser, signOut };
