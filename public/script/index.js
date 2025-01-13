bf6253f6f114cee4de3def974a3f3c23b9d1efd8
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-13 13:41:14 +0530

    scroll animation set

--- public/script/index.js
+++ public/script/index.js
@@ -1,24 +1,15 @@
+
 document.addEventListener('scroll', () => {
-    const aboutSection = document.querySelector('.about-auctionoasis-container');
-    const rect = aboutSection.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
+    const elements = document.querySelectorAll('.scroll-reveal'); 
 
-    if (rect.top <= windowHeight - 100) {
-        aboutSection.classList.add('fade-in');
-    }
+    elements.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top <= windowHeight - 100) {
+            element.classList.add('fade-in');
+        }
+    });
+
+   
 });
-
-
-
-// hover animation feature items
-
-// document.querySelectorAll('.feature-item').forEach(item => {
-//     item.addEventListener('mouseover', () => {
-//         item.style.transform = 'scale(1.05)';
-//         item.style.transition = 'transform 0.3s ease';
-//     });
-
-//     item.addEventListener('mouseout', () => {
-//         item.style.transform = 'scale(1)';
-//     });
-// });
