40ac65238e4547140a738e30d64ef62c9f8c29a2
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-13 09:12:23 +0530

    wallet controller added

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,5 +2,13 @@ const renderIndex = (req, res) => {
     res.render('index', { username: res.locals.username });
 };
   
-module.exports = { renderIndex };
+const renderWallet = (req, res) => {
+    let availableBalance = 2000;
+    let transactionHistory = [
+        { type: 'Deposit', date: '3/19/2024', amount: '+$5,000', status: 'completed' },
+        { type: 'Withdraw', date: '3/20/2024', amount: '-$2,000', status: 'pending' }
+    ]
+    res.render('wallet', { availableBalance, transactionHistory});
+}
+module.exports = { renderIndex , renderWallet };
   
