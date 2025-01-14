b2a1a3498e97677c4d6cba44956489236ec5e280
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-14 12:05:54 +0530

    user profile render successfuly

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -1,8 +1,16 @@
+const path = require('path');
+const {readFile, writeFile} = require('../utils/fileHandler')
 
+const userDetailsFilePath = path.join(__dirname, '../files/userDetails.json')
 
 const renderUserProfile = (req, res) =>{
-    const user = []
-    console.log(req.params.user)
+    let user = []
+    let userDetails = readFile(userDetailsFilePath)
+    user = userDetails.find(u=>u.username === req.params.user)
+    console.log(user)
+    if(user === undefined){
+        user = []
+    }
     
     res.render('profile', {user})
 }
