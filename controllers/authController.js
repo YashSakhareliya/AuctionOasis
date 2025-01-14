eedec89a4d938bebde84208f16a0e8ecd88170e7
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-14 15:05:03 +0530

    NewUser Details Store in json

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const { v4: uuidv4 } = require("uuid");
 const { readFile, writeFile } = require('../utils/fileHandler');
 
 const loginFilePath = path.join(__dirname, '../files/login.json');
 const registeruserFilePath = path.join(__dirname, '../files/registeruser.json');
+const newUserStoreDetailsFilePath = path.join(__dirname, '../files/userDetails.json');
 
 // JWT Secret Key
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -69,7 +71,14 @@ const registerUser = (req, res) => {
   if(existUser){
     return res.send('Username already exists');
   }else{
+    
+    // Store all details of user in json file 
+    newUserStoreDetails(newUser)
+    
+
+    // store detais on registered user
     registeredUser.push(newUser)
+    
     writeFile(registeruserFilePath, registeredUser)
     res.render('auth/login', { title: 'Login' });
   }
@@ -77,6 +86,29 @@ const registerUser = (req, res) => {
   console.log({ username, email, password });
   res.redirect('auth/login');
 };
+
+function newUserStoreDetails(newUserInput){
+
+  const User = {
+    _id: uuidv4(), // Generate a unique ID
+    name: newUserInput.name || null,
+    username: newUserInput.username || null,
+    email: newUserInput.email,
+    password: newUserInput.password, // Hash password securely
+    image: null, // Default to null
+    role: newUserInput.role || "buyer", // Default role
+    profilePicture: null, // Default to null
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    myItems: [], // Default empty array
+    bidHistory: [] // Default empty array
+  };
+  const oldDetails = readFile(newUserStoreDetailsFilePath)
+  oldDetails.push(User)
+  writeFile(newUserStoreDetailsFilePath,oldDetails)
+
+  console.log("Updated: user store details")
+}
 
 const signOut = (req, res) => {
   res.clearCookie('auth_token');
