d543ae67b185a2835eb9bf3bbe14bf69c7b1713b
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-15 21:10:39 +0530

    add remaining time calculate function

--- public/script/index.js
+++ public/script/index.js
@@ -13,3 +13,35 @@ document.addEventListener('scroll', () => {
 
    
 });
+
+// convert time into hours ans date format
+function formatTimeRemaining() {
+    const items = document.querySelectorAll('.item-remTime'); 
+
+    items.forEach(item => {
+        const endTime = item.getAttribute('data-endtime'); 
+        const endDate = new Date(endTime); 
+        const currentDate = new Date(); 
+
+
+        const timeDiff = endDate - currentDate;
+
+        if (timeDiff <= 0) {
+            item.textContent = "Expired"; 
+            return;
+        }
+
+       
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+        
+        item.textContent = `${days}D ${hours}H`;
+    });
+}
+
+
+setInterval(formatTimeRemaining, 1000);
+
