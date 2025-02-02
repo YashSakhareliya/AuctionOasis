a6ee75f01f3fa76f9677d6964631af0d756131d3
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-02 08:53:51 +0530

    Profile Btn js add

--- public/script/profile.js
+++ public/script/profile.js
@@ -12,3 +12,25 @@ tabs.forEach(tab=>{
         document.getElementById(target).classList.add("active");
     })
 })
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const editProfileSection = document.getElementById('edit-profile-section')
+    const editProfileBtn = document.getElementById('edit-profile-btn')
+    const editProfileOverlay = document.getElementById('"edit-profile-overlay')
+    const saveProfileBtn = document.getElementById('save-profile')
+    const cancleProfileBtn = document.getElementById('cancel-profile')
+    const profileSection = document.getElementsByClassName('profile-section') 
+    
+    editProfileBtn.addEventListener('click', (e)=>{
+        e.preventDefault()
+        editProfileSection.classList.toggle('hidden')
+        profileSection.classList.toggle('blur-background')
+        editProfileOverlay.style.display = 'block'
+    })
+
+    cancleProfileBtn.addEventListener("click", function () {
+        editProfileSection.classList.add("hidden");
+        profileSection.classList.remove("blur-background"); // Remove the blur effect
+        editProfileOverlay.style.display = "none"; // Hide the overlay
+    });
+})
